e9d7f7781485aa4b3523de7ad60fee93459bf272
Ameer Hamza <hamzanaseer496@gmail.com>
2024-07-04 22:56:14 +0500

    backend complete

--- Backend/db.js
+++ Backend/db.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const mongouri = "mongodb://localhost:27017/iNoteBook";
 
-const connecttomongo = async () => {
+const connectToMongo = async () => {
     try {
         await mongoose.connect(mongouri);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message);
+        // Optionally handle or log the error further
     }
 }
 
-module.exports = connecttomongo;
+module.exports = connectToMongo;
